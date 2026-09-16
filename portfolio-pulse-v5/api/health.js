87e8ddd3ab1898e9770fbc36c79c6709343cdf06
pat-nb-spec <patootom@gmail.com>
2026-09-16 08:44:25 +0700

    Delete portfolio-pulse-v5 directory

--- portfolio-pulse-v5/api/health.js
+++
@@ -1,2 +0,0 @@
-const {KEYS}=require('./_providers'); const send=require('./_send');
-module.exports=async(req,res)=>send(res,200,{ok:true,providers:{twelve:!!KEYS.twelve,finnhub:!!KEYS.finnhub,alpha:!!KEYS.alpha},runtime:'serverless',autoRefreshMs:Number(process.env.AUTO_REFRESH_MS||30000)});
